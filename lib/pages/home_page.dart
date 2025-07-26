98e8f9448f72f9bdd4a03e3da65f0d0bcea0038b
ODLTour <marie.leblanc0410@gmail.com>
2025-07-26 18:05:20 +0200

    feat: bluetooth perm + bluetooth state

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-class MyHomePage extends StatelessWidget {
+import 'dart:async';
+
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BluetoothAdapterState _bluetoothAdapterState = BluetoothAdapterState.unknown;
+  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _adapterStateStateSubscription = FlutterBluePlus.adapterState.listen((
+      state,
+    ) {
+      _bluetoothAdapterState = state;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _adapterStateStateSubscription.cancel();
+    super.dispose();
+  }
+
+  String _getBluetoothStatusMessage(BluetoothAdapterState state) {
+    switch (state) {
+      case BluetoothAdapterState.unavailable:
+        return 'BluetoothAdapterState.unavailable';
+      case BluetoothAdapterState.unauthorized:
+        return 'BluetoothAdapterState.unauthorized';
+      case BluetoothAdapterState.off:
+        return 'BluetoothAdapterState.off';
+      case BluetoothAdapterState.on:
+        return 'BluetoothAdapterState.on';
+      default:
+        return 'default';
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Scanner Bluetooth')),
-        body: Center(
-          child: Text(
-            'Bienvenue dans le scanner bluetooth !',
-            style: TextStyle(fontSize: 20),
-          ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scanner Bluetooth')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _getBluetoothStatusMessage(_bluetoothAdapterState),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: _bluetoothAdapterState == BluetoothAdapterState.on
+                    ? Colors.green
+                    : Colors.red,
+              ),
+            ),
+          ],
         ),
       ),
     );
